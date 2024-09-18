1ef52194f2aa26dbab4aad8c621e2b99ad5fbb32
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-09-18 21:15:00 +0600

    finish login page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const loginBtn = document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const inputNumber = document.getElementById('input-number').value;
+    const inputPin = document.getElementById('input-pin').value;
+    if (inputNumber === '01739255837' && inputPin === '9255') {
+       window.location.href = "home.html";
+    } else{
+        alert("Sorry!! Invalid number and pin")
+    }
+})
